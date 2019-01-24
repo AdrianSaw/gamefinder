1a9424906e7911e631a4ee50122f4bfb66005621
Adrian Sawicki <adrian.sawicki@polcode.net>
2019-01-24 23:56:45 +0100

    faq placeholder ready

--- src/app/core/faq/faq.component.ts
+++ src/app/core/faq/faq.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class FaqComponent implements OnInit {
-
+  oneAtATime: boolean = true;
   constructor() { }
 
   ngOnInit(): void {
