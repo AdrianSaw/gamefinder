9bebdc2289054d0c06d74c162e74dc301bb004b4
Adrian Sawicki <adrian.sawicki@polcode.net>
2019-01-24 08:38:46 +0100

    circle default config CI

--- src/app/core/faq/faq.component.ts
+++ src/app/core/faq/faq.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-faq',
   templateUrl: './faq.component.html',
-  styleUrls: ['./faq.component.scss']
+  styleUrls: ['./faq.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class FaqComponent implements OnInit {
 
